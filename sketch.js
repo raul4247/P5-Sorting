72a634dae89cdab6f62c29a46aee9fcf62b0f4be
unknown <raul4247@hotmail.com>
2019-04-01 16:28:15 -0300

    p5 canvas setup

--- sketch.js
+++ sketch.js
@@ -14,9 +14,29 @@ function draw() {
         stroke("white")
         line(i, canvasHeight, i, canvasHeight - unsorted[i]);
     }
+    bubbleSort(unsorted)
 }
 
 function resetArray() {
     for(let i = 0; i < unsorted.length; i++)
         unsorted[i] = random(canvasHeight)
+}
+
+function bubbleSort(arr) {
+    let swapFlag
+    do {
+        swapFlag = false
+        for (let i=0; i < arr.length-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                swap(arr, i, i+1)
+                swapFlag = true;
+            }
+        }
+    } while (swapFlag)
+}
+
+function swap(arr, p1, p2) {
+    let temp = arr[p1]
+    arr[p1] = arr[p2]
+    arr[p2] = temp
 }

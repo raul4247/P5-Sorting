a2cc9495173163e94150cfcc7a84405fbcd2735e
raul4247 <raulfmansur@hotmail.com>
2019-04-02 22:48:35 -0300

    bubble sort algorithm implemented

--- sketch.js
+++ sketch.js
@@ -1,38 +1,52 @@
 let canvasWidth = 800, canvasHeight = 600
 let unsorted = []
+let speedSlider
+let i, j
 
 function setup() {
     createCanvas(canvasWidth, canvasHeight)
     unsorted = new Array(canvasWidth)
     resetArray()
+
+    speedSlider = createSlider(1, 1001, 101, 100);
 }
 
-function draw() {
-    background(0);
 
-    for(let i = 0; i < unsorted.length; i++) {
-        stroke("white")
-        line(i, canvasHeight, i, canvasHeight - unsorted[i]);
+function draw() {
+    background(0)
+
+    for(let k = 0; k < speedSlider.value(); k++){
+        if(i < unsorted.length){
+            if(j < unsorted.length - i - 1){
+                if (unsorted[j] > unsorted[j+1])
+                    swap(unsorted, j, j + 1)
+                j++
+            }
+            else{
+                i++
+                j = 0
+            }
+        }
     }
-    bubbleSort(unsorted)
+
+    drawArray()
+}
+
+function drawArray() {
+    for(let c = 0; c < unsorted.length; c++) {
+        if(c == j || c == j+1)
+            stroke("red")
+        else
+            stroke("white")
+        line(c, canvasHeight, c, canvasHeight - unsorted[c]);
+    }
 }
 
 function resetArray() {
-    for(let i = 0; i < unsorted.length; i++)
-        unsorted[i] = random(canvasHeight)
-}
-
-function bubbleSort(arr) {
-    let swapFlag
-    do {
-        swapFlag = false
-        for (let i=0; i < arr.length-1; i++) {
-            if (arr[i] > arr[i+1]) {
-                swap(arr, i, i+1)
-                swapFlag = true;
-            }
-        }
-    } while (swapFlag)
+    for(let c = 0; c < unsorted.length; c++)
+        unsorted[c] = random(canvasHeight)
+    i = 0
+    j = 0
 }
 
 function swap(arr, p1, p2) {
